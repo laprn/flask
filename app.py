15cc54efeb9f8c3d57abfb185e1c556676ec1344
laprn <reg@dymcom.net>
2021-07-17 00:00:44 +0900

    initial commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+# from flask_cors import CORS #comment this on deployment
+from api.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+# CORS(app) #comment this on deployment
+api = Api(app)
 
-@app.route('/')
-def index():
-    return 'Hi, Flask!'
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
 
-if __name__ == '__main__':
-    app.run()
+api.add_resource(HelloApiHandler, '/flask/hello')
